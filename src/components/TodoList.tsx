ba9c97edb5408c62a359b2c3803b26c94d2862a2
Vaibhav Matere <vpm00718897@gmail.com>
2022-09-25 12:05:24 +0530

    feat : todo list added

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./styles.css";
+import { Todo } from "./modal";
+import SingleTodo from "./SingleTodo";
+
+interface Props{
+     todos: Todo[];
+     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList:React.FC<Props> = ({todos, setTodos}:Props) => {
+     return (
+          <>
+               <div className="todos">
+                    {todos.map((todo) => (
+                         <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} /> 
+                    ))}
+               </div>
+          </>
+  )
+};
+
+export default TodoList;
